eac9eed52c502754ff6724b74f9bd1053f5e7569
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-09-04 09:56:41 -0700

    initial commit of nu_plugin_len

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use nu_plugin::{serve_plugin, EvaluatedCall, JsonSerializer, LabeledError, Plugin};
+use nu_protocol::{PluginSignature, Type, Value};
+
+struct Len;
+
+impl Len {
+    fn new() -> Self {
+        Self
+    }
+}
+
+impl Plugin for Len {
+    fn signature(&self) -> Vec<PluginSignature> {
+        vec![PluginSignature::build("len")
+            .usage("calculates the length of its input")
+            .input_output_types(vec![(Type::String, Type::Int)])]
+    }
+
+    fn run(
+        &mut self,
+        name: &str,
+        call: &EvaluatedCall,
+        input: &Value,
+    ) -> Result<Value, LabeledError> {
+        assert_eq!(name, "len");
+        match input {
+            Value::String { val, span } => Ok(Value::Int {
+                val: val.len() as i64,
+                span: span.clone(),
+            }),
+            _ => Err(LabeledError {
+                label: "Expected String input from pipeline".to_string(),
+                msg: format!("requires string input; got {}", input.get_type()),
+                span: Some(call.head),
+            }),
+        }
+    }
+}
+
+fn main() {
+    serve_plugin(&mut Len::new(), JsonSerializer)
+}
